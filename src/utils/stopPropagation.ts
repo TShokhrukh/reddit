a921dcfd07f9478639c9551f99d43f51b125ae00
TShokhrukh <shohruh8945@gmail.com>
2022-07-11 22:23:04 +0500

    fix types stopPropagation and preventDefault

--- src/utils/stopPropagation.ts
+++ src/utils/stopPropagation.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 
-export function preventDefault<F extends (e: unknown) => void>(fn: F) {
-  return (event: React.SyntheticEvent) => {
+export function stopPropagation<F extends (e: any) => void>(fn: F) {
+  return (event: React.SyntheticEvent<any>) => {
     event.stopPropagation();
     fn(event); 
   };
