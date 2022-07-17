6fa84be8faa8c647e33f7fe979beebbc54a7d62f
TShokhrukh <shohruh8945@gmail.com>
2022-07-17 17:29:00 +0500

    add router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import { MainPage } from 'pages';
 
 export function App() {
   return (
-    <div className="App">
-      App Components
-    </div>
+    <BrowserRouter>
+      Header
+      <Container>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
