0e8e719ea03316826af7484b05ec302ffd8baa8e
TShokhrukh <shohruh8945@gmail.com>
2022-07-24 19:55:26 +0500

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeRoutes } from 'routes';
+import { theme } from 'theme';
 
 export function App() {
   return (
     <BrowserRouter>
-      <ThemeRoutes />
+      <ThemeProvider theme={theme}>
+        <ThemeRoutes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
