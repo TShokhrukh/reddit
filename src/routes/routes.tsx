6a519cdc9c9c0574c2303b6a844c67da505e5aa0
TShokhrukh <shohruh8945@gmail.com>
2022-08-08 20:49:53 +0500

    add LoginPage

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { MainPage } from 'pages';
 import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from 'pages/login';
 
 export function ThemeRoutes() {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<MainPage />} />
-        <Route path="login" element={'login'} />
+        <Route path="login" element={<LoginPage />} />
       </Route>
       <Route path="*" element={'404'} />
     </Routes>
