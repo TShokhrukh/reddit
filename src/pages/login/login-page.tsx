6f34728a6be021ccc1bd9cf0682ca5ee9824730b
TShokhrukh <shohruh8945@gmail.com>
2022-08-21 21:59:43 +0500

    add LoginPage

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { REDDIT_AUTH_URL } from 'constants/reddit';
-import { Grid } from '@mui/material';
+import { AuthWrapper } from 'components/AuthWrapper';
+import { Button, Typography, useTheme } from '@mui/material';
+import { LocalMessage } from 'modules/local';
 
 export function LoginPage() {
+  const theme = useTheme();
+  // TODO add scopes list
   return (
-    <Grid container direction="column" justifyContent="center" spacing={2}>
-      <Grid item xs={12}>
-
-        <a href={REDDIT_AUTH_URL}>REDDIT_AUTH_URL</a>
-      </Grid>
-    </Grid>
+    <AuthWrapper>
+      <Typography variant="h5" textAlign="center" color={theme.palette.secondary.main} gutterBottom>
+        <LocalMessage code="authTitle" />
+      </Typography>
+      <Typography variant="h5" textAlign="center" mb={2} color={theme.palette.grey[400]} gutterBottom>
+        <LocalMessage code="authSubTitle" />
+      </Typography>
+      <Button href={REDDIT_AUTH_URL} variant="contained" fullWidth>
+        <LocalMessage code="proceed" />
+      </Button>
+    </AuthWrapper>
   );
 }
