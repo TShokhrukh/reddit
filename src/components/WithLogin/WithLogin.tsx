becdadb898d5dd3b7d37bbaf0d33ddb3738ef2b4
TShokhrukh <shohruh8945@gmail.com>
2022-09-11 20:50:37 +0500

    adding a token to the store

--- src/components/WithLogin/WithLogin.tsx
+++ src/components/WithLogin/WithLogin.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { store } from 'store';
 
 interface IProps {
   children: React.ReactElement
 }
 
 export function WithLogin({ children }: IProps) {
-  // eslint-disable-next-line no-constant-condition
-  if (true) {
+  if (!store.reddit.token) {
     return <Navigate to='/login' />;
   }
   return children;
--- src/pages/auth/auth-page.tsx
+++ src/pages/auth/auth-page.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useURLToken } from 'hooks';
 import { Navigate } from 'react-router-dom';
+import { store } from 'store';
 
 export function AuthPage() {
   const [token] = useURLToken();
 
-  if (token) {
-    return <Navigate to='/' />;
+  if (!token) {
+    return (
+      <div>
+        loading...
+      </div>
+    );
   }
 
-  return (
-    <div>
-      loading...
-    </div>
-  );
+  store.reddit.setToken(token);
+  return <Navigate to='/' />;
 }
