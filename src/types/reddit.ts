f017dc860b631a8e2498a0b01b88e55b0a09eec1
TShokhrukh <shohruh8945@gmail.com>
2022-09-18 17:22:05 +0500

    fix interface ApiReddit

--- src/types/reddit.ts
+++ src/types/reddit.ts
@@ -1,11 +1,15 @@
+import { Options } from 'ky';
 
 export interface IApiReddit {
   token: string
 
-  get (url: string, data: object): Promise<unknown>
-  post (url: string, data: object): Promise<unknown>
-  put (url: string, data: object): Promise<unknown>
-  delete (url: string, data: object): Promise<unknown>
-  delete (url: string, data: object): Promise<unknown>
-  patch (url: string, data: object): Promise<unknown>
+  get (url: string, data?: Options): Promise<unknown>
+  post (url: string, data?: Options): Promise<unknown>
+  put (url: string, data?: Options): Promise<unknown>
+  delete (url: string, data?: Options): Promise<unknown>
+  patch (url: string, data?: Options): Promise<unknown>
+}
+
+export interface IReddit {
+  new: IApiReddit
 }
