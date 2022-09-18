7f1f046040f3edbc4ea4159fa95bf77344b7dadb
TShokhrukh <shohruh8945@gmail.com>
2022-09-18 18:01:48 +0500

    update model ApiReddit

--- src/models/api-reddit.ts
+++ src/models/api-reddit.ts
@@ -1,24 +1,45 @@
 import { KyInstance } from 'ky/distribution/types/ky';
-import ky from 'ky';
+import ky, { Options } from 'ky';
 import { REDDIT_API_URL } from 'constants/reddit';
+import { IApiReddit } from 'types';
 
 interface ICreateFetchOptions {
   prefix?: string
 }
 
-export abstract class ApiReddit {
-  protected prefixUrl: string;
+export abstract class ApiReddit implements IApiReddit {
+  static REDDIT_API_URL: string = REDDIT_API_URL;
+
   public token: string;
-  abstract fetch: KyInstance;
+  protected abstract fetch: KyInstance;
 
   constructor (token: string) {
-    this.prefixUrl = REDDIT_API_URL;
     this.token = token;
+  }
+
+  public get (url: string, data?: Options) {
+    return this.fetch.get(url, data).json();
+  }
+
+  public post (url: string, data?: Options) {
+    return this.fetch.post(url, data).json();
+  }
+
+  public put (url: string, data?: Options) {
+    return this.fetch.put(url, data).json();
+  }
+
+  public delete (url: string, data?: Options) {
+    return this.fetch.delete(url, data).json();
+  }
+
+  public patch (url: string, data?: Options) {
+    return this.fetch.patch(url, data).json();
   }
 
   protected createFetch ({ prefix = '' }: ICreateFetchOptions): KyInstance {
     return ky.create({
-      prefixUrl: this.prefixUrl + prefix,
+      prefixUrl: ApiReddit.REDDIT_API_URL + prefix,
       headers: {
         Authorization: `bearer ${this.token}`
       }
